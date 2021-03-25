8dcfc73530b6f8b9acd27b813e173b31453e933c
Thaila52 <thaila.grag@gmail.com>
2021-03-24 23:32:26 -0300

    operações com array min, max, average e filter

--- src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -13,6 +13,15 @@ fun main(args: Array<String>) {
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
     println("...........................")
     salariosMaiorQue2500.forEach { println(it) }
+    println("...........................")
+
+    println(salarios.count { it in 2000.0..5000.0 })
+
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 500.0 })
+    println("...........................")
+    println(salarios.any { it == 1000.0 })
+
 
 
 }
